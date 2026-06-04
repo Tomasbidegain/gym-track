da06ec1944ff0d8a64b3d852fae5255a9efb84af
Bidegain Tomás <tomasbidegain12@gmail.com>
2026-06-04 10:09:08 -0300

    feat(app): integrate AuthContext and RootNavigator in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AuthContextProvider } from './src/presentation/context/AuthContext';
+import { RootNavigator } from './src/presentation/navigation/RootNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <AuthContextProvider>
+      <RootNavigator />
       <StatusBar style="auto" />
-    </View>
+    </AuthContextProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
