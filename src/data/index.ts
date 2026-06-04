2be3aa698ccff4b464a7a041c9126460071fe487
Bidegain Tomás <tomasbidegain12@gmail.com>
2026-06-04 01:02:49 -0300

    feat(data): add FirestoreUserMetadataRepository implementation

---
+++ src/data/index.ts
@@ -0,0 +1,5 @@
+export { app, auth, db } from './firebase/firebaseConfig';
+
+export { FirebaseAuthRepository } from './firebase/auth/FirebaseAuthRepository';
+export { FirestoreExerciseRepository } from './firebase/firestore/FirestoreExerciseRepository';
+export { FirestoreUserMetadataRepository } from './firebase/firestore/FirestoreUserMetadataRepository';
